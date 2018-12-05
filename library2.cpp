c355c9c6765f57b1727ae65d5eab44529f55311e
englender <45541526+englender@users.noreply.github.com>
2018-12-05 13:26:01 +0200

    Add files via upload

---
+++ library2.cpp
@@ -0,0 +1,17 @@
+//
+// Created by ronien on 05/12/2018.
+//
+
+#include "library2.h"
+#include "ImageTagger.h"
+
+void *Init(int segments){
+    ImageTagger<int,Image>* DS;
+
+    try {
+        DS  = new ImageTagger(segments);
+    }catch  (std::exception& e){
+        return NULL;
+    }
+    return (void*)DS;
+}
