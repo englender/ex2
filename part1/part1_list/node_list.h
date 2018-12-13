44444ba93c68f2158af899f50fd550c1608d301e
englender <ronien262@gmail.com>
2018-12-13 14:15:13 +0200

    updated from part2 changes

--- part1/part1_list/node_list.h
+++ part1/part1_list/node_list.h
@@ -1,4 +1,3 @@
-
 //
 // Created by nadav on 11/28/2018.
 //
@@ -7,6 +6,7 @@
 #define EX2_MAP_LIST_H
 
 #include <stdexcept>
+//#include "library1_for_list.h"
 #include <iostream>
 using namespace std;
 
@@ -20,19 +20,56 @@ class ListNode{
 public:
     ListNode(const K& key, const D& data);
 
+/*
+ * Description: gets the node's key
+ * Input:   none.
+ * Output:  the node's key
+ */
     const K& get_key();
+
+/*
+ * Description: gets the node's data
+ * Input:   none.
+ * Output:  the node's data
+ */
     const D& get_data();
 
+/*
+ * Description: gets the node's next_node
+ * Input:   none.
+ * Output:  the node's next_node
+ */
     ListNode* get_next();           //check about constant!!!
+
+/*
+ * Description: gets the node's prev_node
+ * Input:   none.
+ * Output:  the node's prev_node
+ */
     ListNode* get_prev();
+
+/*
+ * Description: sets the node's next_node
+ * Input:   next - the requested node's next_node
+ * Output:  none.
+ */
     void set_next(ListNode* next);
+
+/*
+ * Description: sets the node's prev_node
+ * Input:   previous - the requested node's prev_node
+ * Output:  none.
+ */
     void set_prev(ListNode* previous);
 
+/*
+ * Description: inside function for printing
+ */
     ostream& printNode(ostream& os);
 
 };
 
-
+//------------------------------------------------------------------------------
 template <class K, class D>
 class Node_list{
     ListNode<K,D>* first;
@@ -41,26 +78,63 @@ public:
     Node_list();
     ~Node_list();
 
+/*
+ * Description: gets the lists first node
+ * Input:   none.
+ * Output:  the lists first node
+ */
     ListNode<K,D>* get_first();
+
+/*
+ * Description: sets the lists first node
+ * Input:   n - the requested node to inserts to list first
+ * Output:  none
+ */
     void set_first(ListNode<K,D>* n);
+
+/*
+ * Description: gets the list's size
+ * Input:   none.
+ * Output:  the tree's size
+ */
     int get_size() const;
 
-    ListNode<K,D>* find(const K& key);
+/*
+ * Description: adds a new node to the list
+ * inserts the node in the beginning of the list
+ * Input:   key - the key of new node to add
+ *          data - the data of new node to add
+ * Output:  none
+ */
     void add_node(const K& key, const D& data);
+
+/*
+ * Description: find's the node with the given key, using recurse finc find_recurse
+ * Input:   key - the key of the node to find
+ * Output:  result - the node with the given key. if there is no node in the tree with the key, returns null
+ */
+    ListNode<K,D>* find(const K& key);
+
+/*
+ * Description: removes the node from the list
+ * Input:   node_ptr - the required node to remove.
+ * Output:  none.
+ */
     void remove_node(ListNode<K,D>* node_ptr);
 
+/*
+ * Description: inside function for printing
+ */
     ostream& printList(ostream& os);
+
 };
 
-
-
 //----------------------------------------------------------------------------//
-//-------------------------Implement of Node class----------------------------//
+//------------------------------ListNode Implement--------------------------------//
 //----------------------------------------------------------------------------//
 template <class K, class D>
 ListNode<K,D>::ListNode(const K& key, const D& data) : key(key), data(data),
-                                                       next_node(nullptr),
-                                                       prev_node(nullptr){
+                                               next_node(nullptr), prev_node(nullptr){
 }
 
 template <class K, class D>
@@ -96,12 +170,9 @@ void ListNode<K,D>::set_prev(ListNode<K,D> *previous) {
 template <class K, class D>
 ostream& ListNode<K,D>::printNode(ostream& os){
     return os <<"Key: " << this->get_key() << endl;
+
 }
-
-
-//----------------------------------------------------------------------------//
-//------------------------Implement of List class-----------------------------//
-//----------------------------------------------------------------------------//
+//-------------------------------------------------------------------------------
 
 template <class K, class D>
 Node_list<K,D>::Node_list() : first(nullptr), size(0) {
@@ -113,7 +184,7 @@ Node_list<K,D>::~Node_list() {
     ListNode<K,D>* next_node;
     while(node_to_delete!= nullptr){
         next_node=node_to_delete->get_next();
-        delete *node_to_delete;
+        delete node_to_delete;
         node_to_delete=next_node;
     }
 }
@@ -128,8 +199,13 @@ void Node_list<K,D>::set_first(ListNode<K,D>* n){
 }
 
 template <class K, class D>
-int Node_list<K,D>::get_size() const {
-    return this->size;
+void Node_list<K,D>::add_node(const K& key, const D& data) {
+    ListNode<K,D>* new_node = new ListNode<K,D>(key, data);
+    new_node->set_next(this->first);
+    if(this->first!= nullptr)
+        this->first->set_prev(new_node);
+    this->first=new_node;
+    this->size++;
 }
 
 template <class K, class D>
@@ -139,16 +215,6 @@ ListNode<K,D>* Node_list<K,D>::find(const K& key){
         result=result->get_next();
     }
     return result;
-}
-
-template <class K, class D>
-void Node_list<K,D>::add_node(const K& key, const D& data) {
-    ListNode<K,D>* new_node = new ListNode<K,D>(key, data);
-    new_node->set_next(this->first);
-    if(this->first!= nullptr)
-        this->first->set_prev(new_node);
-    this->first=new_node;
-    this->size++;
 }
 
 template <class K, class D>
@@ -163,7 +229,13 @@ void Node_list<K,D>::remove_node(ListNode<K,D>* node_ptr){
     if(this->get_first()==node_ptr){
         this->set_first(node_ptr->get_next());
     }
+    delete node_ptr;
     this->size--;
+}
+
+template <class K, class D>
+int Node_list<K,D>::get_size() const {
+    return this->size;
 }
 
 template <class K, class D>
@@ -176,5 +248,6 @@ ostream& Node_list<K,D>::printList(ostream& os){
         current_node=current_node->get_next();
     }
     return os;
-}
+};
+
 #endif //EX2_MAP_LIST_H
--- part1/part1_tree/map_tree.h
+++ part1/part1_tree/map_tree.h
@@ -7,7 +7,8 @@
 
 #include <iostream>
 #include <algorithm>
-
+//#include "map_tree.cpp"
+//#include "library1.h"
 
 using namespace std;
 #define MAX_BF 2
@@ -24,22 +25,90 @@ class TreeNode{
     TreeNode* right_son;
 public:
     TreeNode(const K& key, const D& data);
+//    ~TreeNode();
 
+/*
+ * Description: gets the node's key
+ * Input:   none.
+ * Output:  the node's key
+ */
     const K& get_key();
-    const D& get_data();
+
+/*
+ * Description: gets the node's data
+ * Input:   none.
+ * Output:  the node's data
+ */
+    D& get_data();
+
+/*
+ * Description: gets the node's height
+ * Input:   none.
+ * Output:  the node's height
+ */
     const int get_height();
 
+/*
+ * Description: gets the node's left son
+ * Input:   none.
+ * Output:  the node's left son
+ */
     TreeNode* get_left_son();
+
+/*
+ * Description: gets the node's right son
+ * Input:   none.
+ * Output:  the node's right son
+ */
     TreeNode* get_right_son();
 
+/*
+ * Description: sets the node's left son
+ * Input:   left_son - the node's new left son
+ * Output:  none.
+ */
     void set_left_son(TreeNode* left_son);
+
+/*
+ * Description: sets the node's right son
+ * Input:   right_son - the node's new right son
+ * Output:  none.
+ */
     void set_right_son(TreeNode* right_son);
+
+/*
+ * Description: sets the node's height - the max height between his sons +1
+ * if the node has no sons - height is set to 0
+ * Input:   none.
+ * Output:  none.
+ */
     void set_height();
 
-    int get_right_height();         //return height of the right son
-    int get_left_height();          //return height of the left son
+/*
+ * Description: gets the node's right son height
+ * Input:   none.
+ * Output:  the node's right son height
+ */
+    int get_right_height();
+
+/*
+ * Description: gets the node's left son height
+ * Input:   none.
+ * Output:  the node's left son height
+ */
+    int get_left_height();
+
+/*
+ * Decription: returns node's balance factor - the difference between left son height and right son height
+ * Input:   none.
+ * Output:  node's balance factor
+ */
     int get_balance_factor();
 
+/*
+ * Description: inside function for printing
+ * prints "Key: XXX | Left: XXX | Right: XXX | Height: XXX | BF: XXX "
+ */
     ostream& printNode(ostream& os);
 
 };
@@ -52,50 +121,217 @@ class Map_tree {
 public:
     Map_tree();
     ~Map_tree();
+
+/*
+ * Description: removes all nodes in the tree
+ * function called from Map_tree destructor
+ * Input:   node_to_delete - the tree's root
+ * Output:  none.
+ */
     void delete_recurse(TreeNode<K,D>* node_to_delete);
 
+/*
+ * Description: gets the tree's root
+ * Input:   none.
+ * Output:  the tree's root
+ */
     TreeNode<K,D>* get_root();
+
+/*
+ * Description: sets the tree's root
+ * Input:   the tree's new root to set
+ * Output:  none.
+ */
     void set_root(TreeNode<K,D>* new_root);
 
+/*
+ * Description: gets the tree's size
+ * Input:   none.
+ * Output:  the tree's size
+ */
     int get_size() const;
 
+/*
+ * Description: find's the node with the given key, using recurse finc find_recurse
+ * Input:   key - the key of the node to find
+ * Output:  the node with the given key. if there is no node in the tree with the key, returns null
+ */
     TreeNode<K,D>* find(const K& key);
+
+/*
+ * Description: finds the node with the sent key
+ * Input:   key - the requested key to find
+ *          current_node - first iteration the tree's root, afterwards the current node
+ * Output:  node_ptr - the node with the same key
+ *          if there is no node with the key in the tree, returns nullptr
+ */
     TreeNode<K,D>* find_recurse(const K& key, TreeNode<K,D>* current_node);
+
+/*
+ * Description: finds the node's father and adds it to the tree as his son
+ * corrects the BF of the tree after adding the node, and updates the trees size
+ * Input:   key - the key of new node to add
+ *          data - the data of new node to add
+ * Output:  none
+ */
     void add_node(const K& key,const D& data);
+
+/*
+ * Description: finds the node to remove's father and removes him as his son
+ * then removes the node from the tree
+ * corrects BF of tree after removing the node
+ * Input:   node_to_remove - the required node to remove.
+ * Output:  none.
+ */
     void remove_node(TreeNode<K,D>* node_to_remove);
 
+/*
+ * Description: corrects the BF of the tree after adding a new node
+ * Input:   key - the key of the new node that was added
+ *          current_node - first iteration the tree's root, afterwards the current node
+ *          papa - first iteration nullptr, afterwards the current nodes father
+ * Output:  none.
+ */
     void add_correct(const K& key, TreeNode<K,D>* current_node, TreeNode<K,D>* papa);
+
+/*
+ * Description: If a node has BF of 2, executes roll to the left
+ * is called in add_correct/delete_correct after adding/removing a node
+ * checks if need to roll LL or LR
+ * Input:   current_node - the node with BF = 2
+ * Output:  the new root of the tree that had BF = 2
+ */
     TreeNode<K,D>* roll_left(TreeNode<K,D>* current_node);
+
+/*
+ * Description: If a node has BF of -2, executes roll to the right
+ * is called in add_correct/delete_correct after adding/removing a node
+ * checks if need to roll RL or RR
+ * Input:   current_node - the node with BF = -2
+ * Output:  the new root of the tree that had BF = -2
+ */
     TreeNode<K,D>* roll_right(TreeNode<K,D>* current_node);
+
+/*
+ * Description: If node has BF = 2, and his left son has BF >= 0
+ * Input:   b - is root of mini-tree, BF = 2
+ *          a - is left son of b, BF >=0 (a<b)
+ * Output:  a - new root of mini-tree
+ */
     TreeNode<K,D>* roll_LL(TreeNode<K,D>* b, TreeNode<K,D>* a);
+
+/*
+ * Description: If node has BF = -2, and his left son has BF >= 0
+ * Input:   a - is root of mini-tree, BF = -2
+ *          b - is right son of a, BF <=0 (a<b)
+ * Output:  b - new root of mini-tree
+ */
     TreeNode<K,D>* roll_RR(TreeNode<K,D>* b, TreeNode<K,D>* a);
+
+/*
+ * Description: If node has BF = 2, and his left son has BF >= 0
+ * Input:   c - is root of mini-tree, BF = 2
+ *          a - is left son of c, BF = -1
+ *          b - is right son of a (a<b<c)
+ * Output:  b - new root of mini-tree
+ */
     TreeNode<K,D>* roll_LR(TreeNode<K,D>* c, TreeNode<K,D>* b, TreeNode<K,D>* a);
+
+/*
+ * Description: If node has BF = -2, and his left son has BF = 1
+ * Input:   a - is root of mini-tree, BF = -2
+ *          c - is right son of a, BF = 1
+ *          b - is left son of c (a<b<c)
+ * Output:  b - new root of mini-tree
+ */
     TreeNode<K,D>* roll_RL(TreeNode<K,D>* c, TreeNode<K,D>* b, TreeNode<K,D>* a);
+
+/*
+ * Description: receives a father node and son node and connects between them
+ * if key_son<key_papa then connects tmp_son as left son, else as right son
+ * is called after removing a node or after executing a roll
+ * Input:   papa - the father node of tmp son
+ *          tmp_son - the son of papa
+ * Output:  none.
+ */
     void reconnect_to_papa(TreeNode<K,D>* papa, TreeNode<K,D>* tmp_son);
+
+/*
+ * Description: finds the father of the required node
+ * Input:   key - key of node to add/remove
+ *          current_node - first iteration the tree's root, afterwards the current node
+ * Output:  papa - the father of the node to add/remove
+ */
     TreeNode<K,D>* find_papa(const K& key, TreeNode<K,D>* current_node);
+
+/*
+ * Description: inside function for printing
+ */
     ostream& printTree(ostream& os);
+
+/*
+ * Description: inside function for printing. recursive func called from printTree
+ */
     ostream& print(ostream& os, TreeNode<K,D>* node_to_print);
 
+/*
+ * Description: deletes the node_to_remove and updates the fathers son to nullptr
+ * Input:   papa - the node_to_remove father
+ *          node_to_remove - the node that is to be removed from the tree
+ * Output:  none.
+ */
     void remove_son_with_no_grandsons(TreeNode<K,D>* papa, TreeNode<K,D>* node_to_remove) ;
+
+/*
+ * Description: deletes the node_to_remove and updates the fathers son accordingly
+ * Input:   papa - the node_to_remove father
+ *          node_to_remove - the node that is to be removed from the tree
+ * Output:  none.
+ */
     void remove_son_with_one_grandson(TreeNode<K,D>* papa, TreeNode<K,D>* node_to_remove) ;
+
+/*
+ * Description: deletes the node_to_remove and updates the fathers son accordingly
+ * Input:   papa - the node_to_remove father
+ *          node_to_remove - the node that is to be removed from the tree
+ * Output:  none.
+ */
     void remove_son_with_two_grandson(TreeNode<K,D>* papa, TreeNode<K,D>* node_to_remove) ;
+
+/*
+ * Description: receives two nodes and swaps their right sons
+ * Input:   a - the node_to_remove
+ *          b - the node_to_switch
+ * Output:  none.
+ */
     void swap_right_sons(TreeNode<K,D>* a,TreeNode<K,D>* b);
 
+/*
+ * Description: corrects the BF of the tree after removing a node
+ * Input:   key - the key of the node that was removed
+ *          current_node - first iteration the tree's root, afterwards the current node
+ *          papa - first iteration nullptr, afterwards the current nodes father
+ * Output:  none.
+ */
     void delete_correct(const K& key, TreeNode<K,D>* current_node, TreeNode<K,D>* papa);
 
-};
+    };
 
 
 //----------------------------------------------------------------------------//
-//-------------------------Implement of Node class----------------------------//
+//------------------------------TreeNode Implement--------------------------------//
 //----------------------------------------------------------------------------//
-
 template <class K, class D>
-TreeNode<K,D>::TreeNode(const K& key, const D& data) : key(key), data(data),
-                                                       height(0),
-                                                       left_son(nullptr),
-                                                       right_son(nullptr){
+TreeNode<K,D>::TreeNode(const K& key, const D& data) : key(key), data(data), height(0),
+                                               left_son(nullptr),
+                                               right_son(nullptr){
 }
+
+//template <class K, class D>
+//TreeNode<K,D>::~TreeNode(){
+//    delete &data;
+//    delete &key;
+//}
 
 template <class K, class D>
 const K& TreeNode<K,D>::get_key() {
@@ -103,13 +339,8 @@ const K& TreeNode<K,D>::get_key() {
 }
 
 template <class K, class D>
-const D& TreeNode<K,D>::get_data() {
+D& TreeNode<K,D>::get_data() {
     return this->data;
-}
-
-template <class K, class D>
-const int TreeNode<K,D>::get_height(){
-    return this->height;
 }
 
 template <class K, class D>
@@ -153,7 +384,10 @@ void TreeNode<K,D>::set_height(){
     this->height=(std::max(left_height,right_height)+1);
 }
 
-
+template <class K, class D>
+const int TreeNode<K,D>::get_height(){
+   return this->height;
+}
 
 template <class K, class D>
 int TreeNode<K,D>::get_right_height(){
@@ -195,8 +429,10 @@ ostream& TreeNode<K,D>::printNode(ostream& os) {
     return os;
 }
 
+
+
 //----------------------------------------------------------------------------//
-//-------------------------Implement of Tree class----------------------------//
+//------------------------------TreeNode Tree Implement---------------------------//
 //----------------------------------------------------------------------------//
 
 template <class K, class D>
@@ -208,16 +444,6 @@ Map_tree<K,D>::~Map_tree() {
     delete_recurse(this->root);
 }
 
-template <class K, class D>
-void Map_tree<K,D>::delete_recurse(TreeNode<K,D>* node_to_delete){
-    if(node_to_delete== nullptr)
-        return;
-
-    delete_recurse(node_to_delete->get_left_son());
-    delete_recurse(node_to_delete->get_right_son());
-    delete node_to_delete;
-
-}
 template <class K, class D>
 TreeNode<K,D>* Map_tree<K,D>::get_root(){
     return this->root;
@@ -239,28 +465,19 @@ TreeNode<K,D>* Map_tree<K,D>::find(const K& key){
 
 template <class K, class D>
 TreeNode<K,D>* Map_tree<K,D>::find_recurse(const K& key, TreeNode<K,D>* current_node) {
-    if (current_node == nullptr)
+    if (current_node == nullptr)                //stop conditions - arrived at bottom of the tree
         return nullptr;
 
-    else if (current_node->get_key() == key)
+    else if (current_node->get_key() == key)    //stop conditions - found the required node
         return current_node;
 
     TreeNode<K, D> *node_ptr;
-    if (current_node->get_key() > key) {
+    if (current_node->get_key() > key) {        //the required key is smaller than the current, continue left
         node_ptr = find_recurse(key, current_node->get_left_son());
-//        if (node_ptr == nullptr)
-//            return current_node;
-//
-//        return node_ptr;
-
-    } else {
+    } else {                                    //the required key is bigger than the current, continue right
         node_ptr=find_recurse(key, current_node->get_right_son());
-//        if (node_ptr == nullptr)
-//            return current_node;
-//
-//        return node_ptr;
     }
-    return node_ptr;
+    return node_ptr;                            // ?????? roni - not sure we need this ??????
 }
 
 template <class K, class D>
@@ -271,17 +488,17 @@ TreeNode<K,D>* Map_tree<K,D>::find_papa(const K& key, TreeNode<K,D>* papa) {
     if(papa->get_right_son()== nullptr && papa->get_left_son()== nullptr)
         return papa;
     if(papa->get_key()<key && papa->get_right_son()!= nullptr &&
-       papa->get_right_son()->get_key()==key)
+            papa->get_right_son()->get_key()==key)
         return papa;                //papa_key<key and also it has right_son_key==key
     if(papa->get_key()<key && papa->get_right_son()== nullptr)
         return papa;                //if papa_key<key and also it has no right son
     if(papa->get_key()>key && papa->get_left_son()!= nullptr &&
-       papa->get_left_son()->get_key()==key)
+            papa->get_left_son()->get_key()==key)
         return papa;                //papa_key>key and also it has left_son_key==key
     if(papa->get_key()>key && papa->get_left_son()== nullptr)
-        return papa;            //if papa_key>key and also it has no left son
+            return papa;            //if papa_key>key and also it has no left son
 
-    if(papa->get_key()<key)
+    if(papa->get_key()<key)         //the required key is bigger than the current, continue right
         return find_papa(key,papa->get_right_son());
 
     return find_papa(key,papa->get_left_son());
@@ -302,6 +519,7 @@ void Map_tree<K,D>::add_node(const K& key,const D& data){
     }
     add_correct(new_node->get_key(),this->get_root(), nullptr);
     this->size++;
+
 }
 
 template <class K, class D>
@@ -309,12 +527,12 @@ void Map_tree<K,D>::remove_node(TreeNode<K,D>* node_to_remove) {
     TreeNode<K,D>* papa=find_papa(node_to_remove->get_key(), this->get_root());
     K* key=new K(node_to_remove->get_key());
     if(node_to_remove->get_right_son()== nullptr &&
-       node_to_remove->get_left_son()== nullptr) {              //no grandsons
+            node_to_remove->get_left_son()== nullptr) {              //no grandsons
 
         remove_son_with_no_grandsons(papa, node_to_remove);
     }
     else if(node_to_remove->get_right_son()== nullptr ||
-            node_to_remove->get_left_son()== nullptr){             //only one grandson
+             node_to_remove->get_left_son()== nullptr){             //only one grandson
         remove_son_with_one_grandson(papa, node_to_remove);
     }else{
         remove_son_with_two_grandson(papa, node_to_remove);
@@ -322,6 +540,17 @@ void Map_tree<K,D>::remove_node(TreeNode<K,D>* node_to_remove) {
     delete_correct(*key,this->get_root(), nullptr);
     delete key;
     this->size--;
+}
+
+template <class K, class D>
+void Map_tree<K,D>::delete_recurse(TreeNode<K,D>* node_to_delete){
+    if(node_to_delete== nullptr)
+        return;
+
+    delete_recurse(node_to_delete->get_left_son());
+    delete_recurse(node_to_delete->get_right_son());
+    delete node_to_delete;
+
 }
 
 template <class K, class D>
@@ -401,6 +630,14 @@ void Map_tree<K,D>::swap_right_sons(TreeNode<K,D>* a,TreeNode<K,D>* b) {
     }
 }
 
+//----------------------------------------------------------------------------//
+//----------------------------------------------------------------------------//
+//----------------------------------------------------------------------------//
+//----------------------------------------------------------------------------//
+//----------------------------------------------------------------------------//
+//----------------------------------------------------------------------------//
+
+
 template <class K, class D>
 ostream& Map_tree<K,D>::printTree(ostream& os) {
     TreeNode<K,D>* current_node=this->get_root();
@@ -435,23 +672,25 @@ void Map_tree<K,D>::add_correct(const K& key, TreeNode<K,D>* current_node, TreeN
     if (current_node->get_key() > key) {
         add_correct(key, current_node->get_left_son(),current_node);
         current_node->set_height();
-        if(current_node->get_balance_factor()==MAX_BF)
-            if(papa== nullptr)
+        if(current_node->get_balance_factor()==MAX_BF) {
+            if (papa == nullptr)
                 this->set_root(roll_left(current_node));
             else {
-                tmp_ptr=roll_left(current_node);
+                tmp_ptr = roll_left(current_node);
                 reconnect_to_papa(papa, tmp_ptr);
             }
+        }
     } else {
         add_correct(key, current_node->get_right_son(),current_node);
         current_node->set_height();
-        if(current_node->get_balance_factor()==MIN_BF)
-            if(papa== nullptr)
+        if(current_node->get_balance_factor()==MIN_BF) {
+            if (papa == nullptr)
                 this->set_root(roll_right(current_node));
             else {
                 tmp_ptr = roll_right(current_node);
                 reconnect_to_papa(papa, tmp_ptr);
             }
+        }
     }
 
 }
@@ -514,7 +753,7 @@ TreeNode<K,D>* Map_tree<K,D>::roll_left(TreeNode<K,D>* current_node){
     }
     else{
         return roll_LR(current_node,current_node->get_left_son()->get_right_son(),
-                       current_node->get_left_son());
+                current_node->get_left_son());
     }
 }
 
@@ -525,7 +764,7 @@ TreeNode<K,D>* Map_tree<K,D>::roll_right(TreeNode<K,D>* current_node){
     }
     else{
         return roll_RL(current_node->get_right_son(),
-                       current_node->get_right_son()->get_left_son(),current_node);
+                current_node->get_right_son()->get_left_son(),current_node);
     }
 }
 
@@ -608,5 +847,4 @@ TreeNode<K,D>* Map_tree<K,D>::roll_RL(TreeNode<K,D>* c, TreeNode<K,D>* b, TreeNo
 
     return b;
 };
-
 #endif //EX2_MAP_TREE_H
